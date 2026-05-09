3e513d1bfa225b45e2a83f608db7e754a9f52aeb
Mehmet Cam <73923157+mehmetcamofficial@users.noreply.github.com>
2026-05-09 22:58:02 +0300

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,8 @@
 export default function App() {
-  const handleScan = (code) => {
-    console.log("PRODUCT ID:", code);
-  };
-
   return (
     <div style={{ padding: 20 }}>
       <h1>💊 Pharmacy MVP</h1>
-
-      <p>Dashboard loading...</p>
+      <p>Dashboard is running 🚀</p>
     </div>
-  );
+  )
 }
