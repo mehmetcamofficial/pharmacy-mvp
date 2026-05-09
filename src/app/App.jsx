1f153067907d149774708f0cc5d6de8cb0583340
Mehmet Cam <73923157+mehmetcamofficial@users.noreply.github.com>
2026-05-09 21:22:41 +0300

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,15 @@
-import CameraView from "../components/camera/CameraView";
+import BarcodeScanner from "../components/scanner/BarcodeScanner";
 
 export default function App() {
+  const handleScan = (code) => {
+    console.log("PRODUCT ID:", code);
+  };
+
   return (
     <div style={{ padding: 20 }}>
       <h1>Pharmacy MVP</h1>
 
-      <CameraView />
+      <BarcodeScanner onScan={handleScan} />
     </div>
   );
 }
