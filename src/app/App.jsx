e33368c8c0f25642e2c283c163e552eb3c37c447
Mehmet Cam <73923157+mehmetcamofficial@users.noreply.github.com>
2026-05-09 23:13:48 +0300

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div style={{ padding: 20 }}>
       <h1>💊 Pharmacy MVP</h1>
-      <p>Deployment stable 🚀</p>
+      <p>System running successfully 🚀</p>
     </div>
   )
 }
